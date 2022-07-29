b6b301c14e7545cdcbedf2c56941cc0e7edb4516
Aleksey Gladkov <ja007-ltag@yandex.ru>
2022-07-29 21:19:02 +0300

    D3.2 Creating a simple view of database models
    Introduction to django.views.

---
+++ simpleapp/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.core.validators import MinValueValidator
+
+
+# Товар для нашей витрины
+class Product(models.Model):
+    name = models.CharField(
+        max_length=50,
+        unique=True,  # товары не должны повторяться
+    )
+
+    description = models.TextField()
+
+    quantity = models.IntegerField(  # количество
+        validators=[MinValueValidator(0)],
+        default=1,
+    )
+
+    # поле категории будет ссылаться на модель категории
+    category = models.ForeignKey(
+        to='Category',
+        on_delete=models.CASCADE,
+        related_name='products',  # все продукты в категории будут доступны через поле products
+    )
+
+    price = models.FloatField(
+        validators=[MinValueValidator(0.0)],
+        default=0,
+    )
+
+    def __str__(self):
+        return f'{self.name.title()}: {self.description[:20]} ({self.price})'
+
+
+# Категория к которой будет привязываться товар
+class Category(models.Model):
+    # названия категорий не должны повторяться
+    name = models.CharField(
+        max_length=100,
+        unique=True,
+    )
+
+    def __str__(self):
+        return f'{self.name.title()}'
