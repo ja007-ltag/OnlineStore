6e0a26ae5ab21d72a6c2555c0c277ce2b041a74d
Aleksey Gladkov <ja007-ltag@yandex.ru>
2022-07-31 11:00:06 +0300

    D3.3 IF and FOR in templates. Using filters

--- simpleapp/views.py
+++ simpleapp/views.py
@@ -1,7 +1,10 @@
+from datetime import datetime
+
 # Импортируем класс, который говорит о том,
 # что в этом представлении будет выводиться список объектов БД
 from django.views.generic import ListView, DetailView
 from .models import Product
+from pprint import pprint
 
 
 class ProductList(ListView):
@@ -21,6 +24,23 @@ class ProductList(ListView):
     # Это имя списка, в котором будут лежать все объекты.
     # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
     context_object_name = 'products'
+
+    # Метод get_context_data позволяет изменить набор данных,
+    # который будет передан в шаблон.
+    def get_context_data(self, **kwargs):
+        # С помощью super мы обращаемся к родительским классам
+        # и вызываем у них метод get_context_data с теми же аргументами,
+        # что и были переданы нам.
+        # В ответ мы должны получить словарь.
+        context = super().get_context_data(**kwargs)
+
+        # К словарю добавим текущую дату в ключ 'time_now'.
+        context['time_now'] = datetime.utcnow()
+
+        # Добавим ещё одну пустую переменную,
+        # чтобы на её примере посмотреть работу ещё одного фильтра.
+        context['next_sale'] = 'Распродажа в среду!'
+        return context
 
 
 class ProductDetail(DetailView):
